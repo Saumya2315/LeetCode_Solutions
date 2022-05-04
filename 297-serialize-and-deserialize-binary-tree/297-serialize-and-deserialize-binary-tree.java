023f277af24d958b99cd162361fa0d3335ccd189
Saumya Agarwal <60340798+Saumya2315@users.noreply.github.com>
2022-05-04 13:54:14 +0530

    Time: 30 ms (35.87%), Space: 55.2 MB (18.31%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,30 @@
+public class Codec 
+{
+    public String serialize(TreeNode root)
+    {
+        if(root==null)
+        {
+            return "X";
+        }
+        String leftSerialized = serialize(root.left);
+        String rightSerialized = serialize(root.right);
+        return root.val + "," + leftSerialized +"," + rightSerialized;
+    }
+    public TreeNode deserialize(String data) 
+    {
+        Queue<String> nodesLeft = new LinkedList<>();
+        nodesLeft.addAll(Arrays.asList(data.split(",")));
+        return deserializeHelper(nodesLeft);
+    }
+    public TreeNode deserializeHelper
+        (Queue<String> nodesLeft)
+    {
+        String valueForNode = nodesLeft.poll();
+        if(valueForNode.equals("X"))
+            return null;
+        TreeNode newNode = new TreeNode(Integer.valueOf(valueForNode));
+        newNode.left = deserializeHelper(nodesLeft);
+        newNode.right = deserializeHelper(nodesLeft);
+        return newNode;
+    }
+}
