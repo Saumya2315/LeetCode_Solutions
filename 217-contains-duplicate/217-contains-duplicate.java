33459d241cbc911a2215faaeb7194a4ae6663f40
Saumya Agarwal <60340798+Saumya2315@users.noreply.github.com>
2022-09-28 14:00:02 +0530

    Time: 20 ms (43.94%), Space: 56.1 MB (88.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -5,10 +5,7 @@ class Solution
         Arrays.sort(nums);
         for (int i = 0; i < nums.length-1; i++) 
         {
-            if(nums[i]==nums[i+1])
-            {
-                return true;
-            }
+            if(nums[i]==nums[i+1]) return true;
         }
         return false;
     }
