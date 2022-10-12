aff7c5ee291c23167cc21e1c7cf6663b804199f5
Saumya Agarwal <60340798+Saumya2315@users.noreply.github.com>
2022-10-12 13:01:27 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (92.41%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,13 +8,10 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        // while(node.next!=null ){
             ListNode previous = node.next;
             int value=node.val;
             node.val=node.next.val;
             node.next.val=value;
             node.next=previous.next;
-        // }
-        // node.next=null;
     }
 }
